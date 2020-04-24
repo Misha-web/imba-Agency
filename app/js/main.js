932bdd9542dab2ad91b240d8e524ebe241b1c68e
Misha-web <60877381+Misha-web@users.noreply.github.com>
2020-04-24 22:50:06 +0300

    block about and fixed header is made

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,24 @@
 $(document).ready(function() {
-	
+	let headerH = $('#header__body').innerHeight();
+	let header = $('#header__inner');
+	let scrollOffset = 0;
+	$(window).on("scroll", function() {
+		scrollOffset = $(this).scrollTop();
+		if (scrollOffset >= headerH) {
+			header.addClass('fixed');
+		} else {
+			header.removeClass('fixed');
+		}
+	});
+
+
+	$("[data-scroll]").on("click", function(event) {
+		let blockId = $(this).data('scroll'),
+			blockOffset = $(blockId).offset().top;
+		$("html, body").animate ({
+			scrollTop: blockOffset
+		},500);
+	});
 });
 
 
